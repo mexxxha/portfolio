a52f4f73e5a340f1543b246220f38ac13d45256e
Mode-s <shota.umemoto7@gmail.com>
2026-09-17 17:13:42 +0900

    fix: blog, workの日付と並びを自動取得

--- src/lib/microcms.ts
+++ src/lib/microcms.ts
@@ -1,4 +1,4 @@
-import { createClient, type MicroCMSImage } from 'microcms-js-sdk';
+import { createClient, type MicroCMSImage, type MicroCMSListContent } from 'microcms-js-sdk';
 
 const client = createClient({
   serviceDomain: import.meta.env.MICROCMS_SERVICE_DOMAIN,
@@ -6,39 +6,33 @@ const client = createClient({
 });
 
 export type Work = {
-  id: string;
   title: string;
   description: string;
-  date: string;
   type: string;
   tech: string[];
   thumbnail: MicroCMSImage;
   liveUrl?: string;
-  order: number;
   slug: string;
   body: string;
-};
+} & MicroCMSListContent;
 
 export type Blog = {
-  id: string;
   title: string;
   description: string;
-  date: string;
   tags?: string[];
   thumbnail?: MicroCMSImage;
-  order: number;
   slug: string;
   body: string;
-};
+} & MicroCMSListContent;
 
 export const getWorks = () =>
   client.getList<Work>({
     endpoint: 'works',
-    queries: { limit: 100, orders: 'order' },
+    queries: { limit: 100, orders: '-publishedAt' },
   });
 
 export const getBlogs = () =>
   client.getList<Blog>({
     endpoint: 'blog',
-    queries: { limit: 100, orders: 'order' },
+    queries: { limit: 100, orders: '-publishedAt' },
   });
